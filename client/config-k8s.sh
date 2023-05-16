f500957c3b220823bc54466c3040762ef0fc3684
ALEXANDER QUADRATUS /Ἀλέξανδρος Κοδρᾶτος/ <uniuuu@vivaldi.net>
2023-05-16 22:48:13 +0700

    patch: k8s clinet setup

---
+++ client/config-k8s.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+set -e
+
+
+
+sed -i "s#'http://zotero.org/'#'http://ds.zotprime:8080/'#g" zotero-client/resource/config.js 
+sed -i "s#'zotero.org'#'ds.zotprime'#g" zotero-client/resource/config.js
+sed -i "s#https://api.zotero.org/#http://ds.zotprime:8080/#g" zotero-client/resource/config.js
+sed -i "s#wss://stream.zotero.org/#ws://st.zotprime:8081/#g" zotero-client/resource/config.js
+sed -i "s#https://www.zotero.org/#http://ds.zotprime:8080:8080/#g" zotero-client/resource/config.js
+sed -i "s#https://zoteroproxycheck.s3.amazonaws.com/test##g" zotero-client/resource/config.js
+
+
+
