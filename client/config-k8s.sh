a8439bd7da567c0e3f0eb690b9ad4c28f7ba5f11
ALEXANDER QUADRATUS /Ἀλέξανδρος Κοδρᾶτος/ <uniuuu@vivaldi.net>
2023-05-24 06:01:20 +0700

    patch: updated GKE, microk8s charts

--- client/config-k8s.sh
+++ client/config-k8s.sh
@@ -3,8 +3,8 @@
 set -eux
 
 
-#sed -i "s#'http://zotero.org/'#'http://ds.zotprime:8080/'#g" zotero-client/resource/config.js 
-#sed -i "s#'zotero.org'#'ds.zotprime'#g" zotero-client/resource/config.js
+#sed -i "s#'http://zotero.org/'#'http://api.projectdev.net/'#g" zotero-client/resource/config.js 
+#sed -i "s#'zotero.org'#'api.projectdev.net'#g" zotero-client/resource/config.js
 sed -i "s#https://api.zotero.org/#http://api.projectdev.net/#g" zotero-client/resource/config.js
 sed -i "s#wss://stream.zotero.org/#ws://stream.projectdev.net/#g" zotero-client/resource/config.js
 sed -i "s#https://www.zotero.org/#http://api.projectdev.net/#g" zotero-client/resource/config.js
