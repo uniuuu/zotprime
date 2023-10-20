6bc7324fbda2a0889952a23de468b2e833133945
ALEXANDER QUADRATUS /Ἀλέξανδρος Κοδρᾶτος/ <uniuuu@vivaldi.net>
2023-10-20 23:44:45 +0700

    fix: Add user

--- docker/dataserver/create-user.sh
+++ docker/dataserver/create-user.sh
@@ -8,5 +8,5 @@ echo "INSERT INTO users VALUES ('${1}', '${5}', '${2}')" | $MYSQL zotero_master
 echo "INSERT INTO groupUsers VALUES ('1', '${1}', 'member', '0000-00-00 00:00:00', '0000-00-00 00:00:00')" | $MYSQL zotero_master
 echo "INSERT INTO users VALUES ('${1}', '${2}', MD5('${3}'))" | $MYSQL zotero_www
 echo "INSERT INTO users_email (userID, email) VALUES ('${1}', '${4}')" | $MYSQL zotero_www
-echo "INSERT INTO shardLibraries VALUES ('${5}', 'user', '0000-00-00 00:00:00', '0')" | $MYSQL zotero_shard_1
+echo "INSERT INTO shardLibraries VALUES ('${5}', 'user', '0000-00-00 00:00:00', '0', '0')" | $MYSQL zotero_shard_1
 
