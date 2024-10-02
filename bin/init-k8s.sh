f59108e002e27a336d165527f4491144254469c0
ALEXANDER QUADRATUS /Ἀλέξανδρος Κοδρᾶτος/ <uniuuu@vivaldi.net>
2024-10-02 15:20:42 -0400

    patch: Update for K8s - Resolves 'preprint' issue with Arxiv papers. https://github.com/uniuuu/zotprime/issues/14 https://github.com/uniuuu/zotprime/issues/34

--- bin/init-k8s.sh
+++ bin/init-k8s.sh
@@ -7,3 +7,4 @@ kubectl -n zotprime exec -it $(kubectl -n zotprime get pods -l apps=zotprime-dat
 kubectl -n zotprime exec -it $(kubectl -n zotprime get pods -l apps=zotprime-dataserver -o custom-columns=:metadata.name) -- sh -cux 'aws --endpoint-url "http://minio:9000" s3 mb s3://zotero'
 kubectl -n zotprime exec -it $(kubectl -n zotprime get pods -l apps=zotprime-dataserver -o custom-columns=:metadata.name) -- sh -cux 'aws --endpoint-url "http://minio:9000" s3 mb s3://zotero-fulltext'
 kubectl -n zotprime exec -it $(kubectl -n zotprime get pods -l apps=zotprime-dataserver -o custom-columns=:metadata.name) -- sh -cux 'aws --endpoint-url "http://localstack:4575" sns create-topic --name zotero'
+kubectl -n zotprime exec -it $(kubectl -n zotprime get pods -l apps=zotprime-dataserver -o custom-columns=:metadata.name) -- sh -cux 'cd /var/www/zotero/admin && php schema_update'
